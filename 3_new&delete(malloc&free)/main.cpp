d262d206929670bebe91c40ac28272a8dca89493
woosukqw <54995656+woosukqw@users.noreply.github.com>
2022-01-10 02:07:29 +0900

    Create main.cpp

---
+++ 3_new&delete(malloc&free)/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+
+    /* 
+    malloc == new
+    Usage: datatype* pointer = new datatype;
+    
+    free == delete
+    Usage: delete pointer;
+    */
+    int* p = new int;
+    
+  
+    return 0;
+}
