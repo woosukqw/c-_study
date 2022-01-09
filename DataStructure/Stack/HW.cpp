cc9ce6353129ee9684794e2aaba984540551cd35
woosukqw <54995656+woosukqw@users.noreply.github.com>
2022-01-10 03:49:39 +0900

    Create HW.cpp
    HW

---
+++ DataStructure/Stack/HW.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include"Stack.h"
+
+Stack::Stack() {
+    top = -1;
+}
+
+Stack::~Stack(){
+    //
+}
+void Stack::push(int x){
+    //
+}
+
+int Stack::pop(){
+    //
+}
+
+int Stack::peek(){
+    //
+}
+
+bool Stack::isFull() {
+    //
+}
+
+bool Stack::isEmpty() {
+    //
+}
+
+int Stack::size(){
+    //
+}
+
+void Stack::display() {
+    //
+}
+
+int main(){
+    Stack stack;
+    stack.push(5);
+    stack.push(3);
+    stack.display();
+}
