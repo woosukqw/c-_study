1d6a9f09854e85b7d442c22e04ac0b8d4239e07c
woosukqw <54995656+woosukqw@users.noreply.github.com>
2022-01-10 02:08:19 +0900

    Update main.cpp

--- 3_new&delete(malloc&free)/main.cpp
+++ 3_new&delete(malloc&free)/main.cpp
@@ -2,15 +2,17 @@
 
 int main() {
 
-    /* 
+    /*
     malloc == new
     Usage: datatype* pointer = new datatype;
-    
+
     free == delete
     Usage: delete pointer;
     */
     int* p = new int;
-    
-  
+    *p = 10;
+    std::cout << *p << std::endl;
+    delete p;
+
     return 0;
 }
