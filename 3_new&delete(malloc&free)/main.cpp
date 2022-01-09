0f572afb36089e494b206ad608fd260bda6a32ef
woosukqw <54995656+woosukqw@users.noreply.github.com>
2022-01-10 02:34:14 +0900

    Update main.cpp

--- 3_new&delete(malloc&free)/main.cpp
+++ 3_new&delete(malloc&free)/main.cpp
@@ -14,5 +14,19 @@ int main() {
     std::cout << *p << std::endl;
     delete p;
 
+    //new allocating array example.
+    int len = 5;
+    int *parr = new int[len];
+    for (int i=0; i<len; i++){
+        *(parr+i) = i;
+    }
+    for (int i=0; i<len; i++){
+        std::cout << i << "th element of array: " << parr[i] << std::endl;
+    }
+    // new-delete, new[]-delete[]
+    delete[] parr;
+    
+    
+    
     return 0;
 }
