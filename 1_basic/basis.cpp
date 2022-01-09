3fdeadf0a50b4edd6e84a1c1bfcdecfadeb71889
woosukqw <54995656+woosukqw@users.noreply.github.com>
2022-01-10 01:42:26 +0900

    Create basis.cpp

---
+++ 1_basic/basis.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+/*
+ * namespace 속에 있는 값, 함수들을 불러와 사용하기위해?..
+ * cout, cin, endl은 iostream헤더파일의 std라는 namespace에 정의되어 있음.
+ * using namespace std; 하면 cout << "~" << endl; 도 가능, but 권장 안함
+ *
+ * std::cout << 출력할값1 << 2 << ... << n;
+ * std::endl -> 화면에 출력해주는 함수
+ * std::cout << std::endl; -> 엔터 하나 출력
+ *
+ */
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+
+    //printf == std::cout << ~ || scanf== cin >> ~
+    int a, b;
+    //std::cin >> a >> b;
+    //std::cout << a << " " << b << std::endl;
+    // 자료형
+    /*
+    int i;
+    char c;
+    double d;
+    float f;
+     */
+    //포인터 방법 C와 동일
+    int arr[10];
+    int *parr = arr;
+    int i;
+    int *pi = &i;
+
+    //for
+    //int i;
+    for (i = 0; i<10; i++){
+        std::cout << i << " ";
+    }
+    std::cout << std::endl;
+
+    //while
+    i = 0;
+    int sum = 0;
+    while (i <= 10){
+        sum += i;
+        i += 1;
+    }
+    std::cout << "sum: " << sum << std::endl;
+
+    //if
+    if (i == 11){
+        std::cout << "i: 11" << std::endl;
+        i += 1;
+    }
+    else{
+        std::cout << "i != 11" << std::endl;
+    }
+
+    //이외 switch, break, continue 등 C와 동일.
+    //std::cout << &i << std::endl;
+
+    //reference(참조자) &
+    int d = 5;
+    int& ref = d;
+
+    std::cout << d << std::endl;
+    ref = 10;
+    std::cout << d << std::endl;
+
+    //reference of array
+    int arr1[3] = {1,2,3};
+    int(&ref2)[3] = arr1; // ref[0] 이 arr1[0] 의 reference, ``[1]이 ``[1]의 reference ...
+
+    ref2[0] = 3;
+    ref2[2] = 1;
+
+    std::cout << arr1[0] << arr1[1] << arr1[2] << std::endl;
+
+    return 0;
+}
