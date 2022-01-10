4d96cac58d7a79bbaec504dc42b4e90ffac20a38
woosukqw <54995656+woosukqw@users.noreply.github.com>
2022-01-11 03:31:49 +0900

    Create main.cpp

---
+++ 4_Class/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+/*
+
+class
+private: class외부에서 수정 불가
+public : class외부에서 수정 가능
+
+constructor: class obj생성하는거?
+Class01 class(1,2);
+default ``
+Stack stack; <-
+ */
+int main() {
+
+
+    return 0;
+}
