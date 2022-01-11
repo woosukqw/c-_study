0945836d5664c0d9389b1416dcbbf457d1e73b67
woosukqw <54995656+woosukqw@users.noreply.github.com>
2022-01-12 00:07:01 +0900

    Create stack.h

---
+++ DataStructure/balanced_parentheses_easy/stack.h
@@ -0,0 +1,28 @@
+#include<iostream>
+
+inline void error(const char *str){
+    std::cout << str << std::endl;
+    exit(1);
+}
+
+const int MAX_STACK_SIZE = 10;
+
+class Stack{
+private:
+    int top;
+    char* data = new char[MAX_STACK_SIZE];
+
+public:
+    Stack();
+    ~Stack();
+    void reset(){
+        top = -1;
+    }
+    void push(int x);
+    void pop();
+    int peek();
+    bool isFull();
+    bool isEmpty();
+    int size();
+    void display();
+};
