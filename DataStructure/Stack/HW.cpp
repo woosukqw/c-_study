c14884007eaba82cbbb27f2608520fd36af334f1
woosukqw <54995656+woosukqw@users.noreply.github.com>
2022-01-11 15:11:29 +0900

    Update HW.cpp

--- DataStructure/Stack/HW.cpp
+++ DataStructure/Stack/HW.cpp
@@ -4,41 +4,44 @@
 Stack::Stack() {
     top = -1;
 }
-
 Stack::~Stack(){
-    //
-}
-void Stack::push(int x){
-    //
+    delete [] data;
 }
 
-int Stack::pop(){
-    //
+void Stack::push(int x){
+    //TODO
+}
+
+void Stack::pop(){
+    //TODO
 }
 
 int Stack::peek(){
-    //
+    //TODO
 }
 
 bool Stack::isFull() {
-    //
+    //TODO
 }
 
 bool Stack::isEmpty() {
-    //
+    //TODO
 }
 
 int Stack::size(){
-    //
+    //TODO
 }
 
 void Stack::display() {
-    //
+    //TODO
 }
 
 int main(){
     Stack stack;
     stack.push(5);
     stack.push(3);
+    stack.pop();
+    std::cout << "peek of Stack: " << stack.peek() << std::endl;
+    std::cout << "size of Stack: " << stack.size() << std::endl;
     stack.display();
 }
