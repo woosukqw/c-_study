c50256724bceadbb41e45319e855c2ecd5827aa4
woosukqw <54995656+woosukqw@users.noreply.github.com>
2022-01-15 20:17:31 +0900

    Create main.cpp

---
+++ DataStructure/1./hw/스택 구현/main.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include"Stack.h"
+
+Stack::Stack() {
+    top = -1;
+}
+Stack::~Stack(){
+    delete [] data;
+    //data[MAX_STACK_SIZE] = NULL;
+}
+
+void Stack::push(int x){
+    if (isFull()){
+        error("stack full error");
+    }
+    top += 1;
+    data[top] = x;
+}
+
+void Stack::pop(){
+    if (isEmpty()){
+        error("stack empty error");
+    }
+    top -= 1;
+}
+
+bool Stack::isFull() {
+    if (top == MAX_STACK_SIZE)
+        return true;
+    else
+        return false;
+}
+
+bool Stack::isEmpty() {
+    if (top == -1)
+        return true;
+    else
+        return false;
+}
+
+int Stack::size(){
+    return top+1;
+}
+
+void Stack::display() {
+    for (int i=0; i<=top; i++){
+        std::cout << i << "th value: " << data[i] << std::endl;
+    }
+}
+/*
+사용해도 되고, main함수 안에 코딩해도 무관함.
+bool check_brace(char str[]){
+    //TODO
+}
+*/
+int main(){
+    // TODO, 자유롭게 스택 관련 함수로 테스트
+    Stack stack;
+  
+    stack.push(5);
+    stack.push(3);
+    stack.pop();
+    std::cout << "peek of Stack: " << stack.peek() << std::endl;
+    std::cout << "size of Stack: " << stack.size() << std::endl;
+    stack.display();
+
+}
